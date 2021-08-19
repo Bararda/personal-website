894fa38499b95271b5adc3b05b05b7c6345d0eb0
Bararda <anthonydoucet5417@hotmail.com>
2021-08-18 17:27:03 -0700

    file download button

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import SectionHeader from 'components/section-header/section-header';
 import NorthOrca from 'assets/images/norca-logo.png';
 import './experience.css';
+import FileDownload from 'components/file-download/file-download';
 
 export default function Experience() {
 	const content = 'North Orca'; // TO-DO
 	return (
-		<div className="about-me">
+		<div className="experience">
 			<img className="headshot-image" src={NorthOrca} alt="North Orca" />
 			<div>
 				<SectionHeader header="Experience" />
 				{content}
 			</div>
+			<FileDownload filePath="assets/files/resume.docx" proposedFileName="resume">Resume</FileDownload>
 		</div>
 	);
 }
--- src/components/file-download/file-download.jsx
+++ src/components/file-download/file-download.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+import './file-download.css';
 
-export default function FileDownload() {
+export default function FileDownload({ children, filePath, proposedFileName }) {
 	return (
-		<div />
+		<a href={filePath} download={proposedFileName} target="_blank" rel="noreferrer">
+			{children}
+		</a>
 	);
 }
 
 FileDownload.propTypes = {
+	children: PropTypes.element,
+	filePath: PropTypes.string,
+	proposedFileName: PropTypes.string,
 };
 
 FileDownload.defaultProps = {
+	children: undefined,
+	filePath: undefined,
+	proposedFileName: undefined,
 };
