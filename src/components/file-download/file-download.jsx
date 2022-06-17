957d4919546d357c826d89f6367e3bba7117ab9e
Bararda <anthonydoucet5417@hotmail.com>
2022-06-17 15:14:32 -0700

    changed to external pdf hosting

--- src/components/file-download/file-download.jsx
+++ src/components/file-download/file-download.jsx
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types';
 import fileIcon from 'assets/images/file-download.png';
 import './file-download.css';
 
-export default function FileDownload({ children, filePath, proposedFileName }) {
+export default function FileDownload({ children, filePath }) {
 	return (
-		<a href={filePath} download={proposedFileName} target="_blank" rel="noreferrer" className="file-download">
+		<a href={filePath} target="_blank" rel="noreferrer" className="file-download">
 			<img src={fileIcon} alt="download File" />
 			{children}
 		</a>
@@ -19,11 +19,9 @@ FileDownload.propTypes = {
 		PropTypes.arrayOf(PropTypes.node),
 	]),
 	filePath: PropTypes.string,
-	proposedFileName: PropTypes.string,
 };
 
 FileDownload.defaultProps = {
 	children: undefined,
 	filePath: undefined,
-	proposedFileName: undefined,
 };
